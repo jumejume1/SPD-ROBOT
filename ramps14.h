ee1bb04619a9bd6f72ecb777208395eddf95c071
jumejume1 <jumejume1@hotmail.com>
2022-03-14 23:01:40 +0700

    Create ramps14.h

---
+++ ramps14.h
@@ -0,0 +1,15 @@
+
+// Pins to Axis1 RA/Azm on RAMPS X
+#define Axis1DirPin   55    // Pin A1 (Dir)
+#define Axis1StepPin  54    // Pin A0 (Step)
+#define Axis1_EN      38    // Pin 38 (Enable)
+
+// Pins to Axis2 Dec/Alt on RAMPS Y
+#define Axis2DirPin    61   // Pin A7 (Dir)
+#define Axis2StepPin   60   // Pin A6 (Step)
+#define Axis2_EN       56    // Pin A2 (Enable)
+
+// Pins to rotator stepper driver on RAMPS Z
+#define Axis3DirPin   48    // Pin 48 (Dir)
+#define Axis3StepPin  46    // Pin 46 (Step)
+#define Axis3_EN      62    // Pin A8 (Enable)
